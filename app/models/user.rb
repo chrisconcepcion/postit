df2ddda268a3a27f93e1202012f483ed3f894ede
Christopher Concepcion <feverdoingwork@gmail.com>
2013-08-12 18:41:24 -0400

    voting issue

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+	include Slugs
 
 	has_many :posts
 	has_many :comments
@@ -13,6 +14,7 @@ class User < ActiveRecord::Base
 	def two_factor_auth?
 		self.phone.present?
 	end
+
 
 	def generate_pin!
 		self.update_column(:pin, rand(10 ** 6)) 
@@ -42,34 +44,7 @@ class User < ActiveRecord::Base
 		self.votes.where(voteable: obj).size > 0
 	end
 
-	def generate_slug
-	    if self.errors.empty?
-	#strip the string
-	      slug = self.username.dup
 
-	#blow away apostrophes
-	      slug.gsub! /['`]/,""
-
-	# @ --> at, and & --> and
-	      slug.gsub! /\s*@\s*/, " at "
-	      slug.gsub! /\s*&\s*/, " and "
-
-	#replace all non alphanumeric, underscore or periods with underscore
-	      slug.gsub! /\s*[^A-Za-z0-9\.\-]\s*/, '_'  
-
-	#convert double underscores to single
-	      slug.gsub! /_+/,"_"
-
-	#strip off leading/trailing underscore
-	      slug.gsub! /\A[_\.]+|[_\.]+\z/,""
-
-	      slug.downcase!
-
-	      count = 0
-	      final_slug = slug
-	      self.slug = final_slug
-	    end
-	  end
 	  def to_param
 	    self.slug
 	  end
