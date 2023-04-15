c0dc157c58be9ecbb0d37487d1e91786882d0ea2
MacbookPro <macbookpro@macbookpros-mbp.lan>
2023-04-15 13:14:17 -0400

    removed unneeded files

--- c/config/initializers/cookie_verification_secret.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = '7afbd8342315c2dda8d3bc18950ebc0dd0d9d8e52e25f39890be49b62721f15d85a94401f1ca88a43eb2558cae2871e945276bbfce94ba6ea3c746ec2730bfd0';
